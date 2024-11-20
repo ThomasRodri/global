29f3b44006b1d7365f72139daade2b2304e088dc
ThomasRodri <thomas04.rodrigues04@gmail.com>
2024-11-20 20:13:53 -0300

    ajustes nos links do Cabecalho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Cadastro from "@/components/Cadastro";
+
 import Carrossel from "@/components/Carrossel";
-import LoginPage from "@/components/Login";
+
 
 export default function Home() {
   return (
     <div>
       <Carrossel/>
-      <LoginPage/>
-      <Cadastro/>
+      
     </div>
     
   );
